f72f1c6228a951527172ea5c911a472593c6123f
Yusuf GÜREL <44755433+yuckymarrow@users.noreply.github.com>
2020-11-29 23:29:27 +0300

    Add files via upload

---
+++ Kök_Bulma.py
@@ -0,0 +1,17 @@
+print("İkinci Dereceden Denklemin Köklerini Bulma Programı\nMADE BY YUCKYMAN")
+
+
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+
+delta = b ** 2 - 4 * a * c
+
+x1 = (- b - delta ** 0.5) / ( 2 * a )
+x2 = (- b + delta ** 0.5) / (2 *a )
+
+
+print("Birinci Kök: {}\nİkinci Kök: {}\n".format(x1,x2))
+
+#where it all started aga :)
+    
