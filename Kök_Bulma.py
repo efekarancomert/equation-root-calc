2a593351657e5a1cde67a4623a7732350e60173a
efekarancomert <54054797+efekarancomert@users.noreply.github.com>
2020-12-01 13:20:33 +0300

    Update Kök_Bulma.py

--- Kök_Bulma.py
+++ Kök_Bulma.py
@@ -5,13 +5,12 @@ a = int(input("a: "))
 b = int(input("b: "))
 c = int(input("c: "))
 
-delta = b ** 2 - 4 * a * c
+delta = (b ** 2) - (4 * a * c)
 
-x1 = (- b - delta ** 0.5) / ( 2 * a )
-x2 = (- b + delta ** 0.5) / (2 *a )
+x1 = (- b - (delta ** 0.5)) / ( 2 * a )
+x2 = (- b + (delta ** 0.5)) / (2 *a )
 
 
-print("Birinci Kök: {}\nİkinci Kök: {}\n".format(x1,x2))
+print(f"Birinci Kök: {x1}\nİkinci Kök: {x2}")
 
 #where it all started :)
-    
